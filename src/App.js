b983cc43a0ca12973242093fcbdfe4aa70781c44
Nitin Katiyar <mickey@Nitins-MacBook-Air.local>
2020-08-18 17:33:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "react-bootstrap/Card";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class Employee extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      employee: [],
+    };
+  }
+  componentDidMount() {
+    this.fetchEmpData();
+  }
+
+  fetchEmpData() {
+    axios
+      .get("http://dummy.restapiexample.com/api/v1/employees")
+      .then((response) => {
+        console.log(response);
+        if (response.status === 200) {
+          this.setState({
+            employee: response.data.data,
+          });
+        }
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="first_container">
+          {this.state.employee.map((employee, index) => (
+            <Card className="card" key={index}>
+              {/* <Card.Img
+                variant="top"
+                src={employee.profile_image}
+                height={180}
+                width={130}
+              /> */}
+              <Card.Body>
+                <Card.Text className="card_text">
+                  Employee Name: {employee.employee_name}
+                  <br />
+                  Salary: {employee.employee_salary}
+                  <br />
+                  Age: {employee.employee_age}
+                </Card.Text>
+                <button onClick>Remove Employee</button>
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Employee;
